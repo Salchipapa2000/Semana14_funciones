574e4a76dc17f5cc7a0a80d53f228fa8792a3fd1
Jeanneth Alejandra Moran Alvarez <j.alejandramoran@gmail.com>
2025-10-18 08:51:10 -0600

    Versión inicial del programa de funciones

---
+++ funciones.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// Prototipos
+int sumar(int a, int b);
+void intercambiar(int *x, int *y);
+
+int main() {
+    int num1 = 5, num2 = 10;
+    printf("Suma: %d + %d = %d\n", num1, num2, sumar(num1, num2));
+    printf("Antes del intercambio: num1 = %d, num2 = %d\n", num1, num2);
+    intercambiar(&num1, &num2);
+    printf("Después del intercambio: num1 = %d, num2 = %d\n", num1, num2);
+    return 0;
+}
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+void intercambiar(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
