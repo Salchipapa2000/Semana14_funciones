2bf6d518d627dbd359589b5bd50e01e250553f2f
Jeanneth Alejandra Moran Alvarez <j.alejandramoran@gmail.com>
2025-10-18 09:03:06 -0600

    Se agregó función restar()

--- funciones.c
+++ funciones.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 // Prototipos
 int sumar(int a, int b);
+int restar(int a, int b);
 void intercambiar(int *x, int *y);
 
 int main() {
     int num1 = 5, num2 = 10;
     printf("Suma: %d + %d = %d\n", num1, num2, sumar(num1, num2));
+    printf("Resta: %d - %d = %d\n", num1, num2, restar(num1, num2));
     printf("Antes del intercambio: num1 = %d, num2 = %d\n", num1, num2);
     intercambiar(&num1, &num2);
     printf("Después del intercambio: num1 = %d, num2 = %d\n", num1, num2);
@@ -20,4 +22,8 @@ void intercambiar(int *x, int *y) {
     int temp = *x;
     *x = *y;
     *y = temp;
+}
+
+int restar(int a, int b) {
+    return a - b;
 }
